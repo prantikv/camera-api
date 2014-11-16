0d5c996201f60efcee5c50481eba222da87262f2
prantikv <prantikv@yahoo.com>
2014-11-16 04:45:04 -0800

    simple test

--- custom.js
+++ custom.js
@@ -6,23 +6,8 @@ document.addEventListener("deviceready", onDeviceReady, false);
 }
 
 function onDeviceReady() {
-  
+ 
+alert(navigator.camera);
 
-document.getElementById('gallery').addEventListener("click",function(){
-	 alert(22);
-
-	navigator.camera.getPicture(onSuccess, onFail, { quality: 50,destinationType: Camera.DestinationType.FILE_URI });
-});
-
-function onSuccess(imageURI) {
-    var image = document.getElementById('myImage');
-     image.src = imageURI;
-      alert(imageURI);
 }
 
-function onFail(message) {
-    alert('Failed because: ' + message);
-}
-
-
-} 
