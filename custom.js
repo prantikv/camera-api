d79935971470c29280fb260d1ffd9e5c8812bc32
prantikv <prantikv@yahoo.com>
2014-11-16 03:28:00 -0800

    config file changed

--- custom.js
+++ custom.js
@@ -2,8 +2,8 @@ document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
     // Now safe to use device APIs
-    alert("we were called");
-        alert(navigator.camera);
+    // alert("we were called");
+    //     alert(navigator.camera);
 }
 
 
@@ -11,13 +11,11 @@ document.getElementById('gallery').addEventListener('click',takepicture);
 
 
 function takepicture(){
-	//alert(2);
+	alert(2);
 
-	navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
-    destinationType: Camera.DestinationType.FILE_URI });
-
-
+	navigator.camera.getPicture(onSuccess, onFail, { quality: 50,destinationType: Camera.DestinationType.FILE_URI });
 }
+
 function onSuccess(imageURI) {
     var image = document.getElementById('myImage');
     image.src = imageURI;
